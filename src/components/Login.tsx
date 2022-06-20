bcb14df51dbfd43aa62eefce1e56f302eecd7061
marciofsj <97961755+marciofsj@users.noreply.github.com>
2022-06-20 12:18:09 -0300

    final pagina login

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,29 +1,33 @@
+import styles from './Login.module.css'
+
 export function Login() {
     return (
-        
-        <div>
-            <form>
-                <h1>Login</h1>
-                <input type="email" name="E-mail" id="" placeholder="e-mail"/>
-                <input type="password" name="Senha" id="" placeholder="senha"  />
-                <a href="#">Esqueceu sua senha? Clique aqui!</a>
-                <button>Acessar</button>
-            </form>
+        <div className={styles.loginPage}>
+            <div>
+                <form className={styles.loginSide}>
+                    <h2  >Login</h2>
+                    <input type="email" name="E-mail" id="" placeholder="e-mail"/>
+                    <input type="password" name="Senha" id="" placeholder="senha"  />
+                    <a href="#">Esqueceu sua senha? Clique aqui!</a>
+                    <button>Acessar</button>
+                </form>
+            </div>
 
-            <form>
-                <h1>Cadastro</h1>
-                <input type="text" name="Nome" id="" placeholder="nome completo"/>
-                <input type="email" name="E-mail" id="" placeholder="e-mail"/>
-                <input type="password" name="Senha" id="" placeholder="senha"  />
-                <input type="password" name="Confirma" id="" placeholder="confirme a senha"  />
-                
-                <div>
-                    <input type="checkbox" name="Check" id="" />
-                    <p>Quero receber atualizações e promoções por e-mail.</p>
-                </div>
-                <button>Acessar</button>
-            </form>
-
+            <div>
+                <form className={styles.registerSide}>
+                    <h2>Cadastro</h2>
+                    <input type="text" name="Nome" id="" placeholder="nome completo"/>
+                    <input type="email" name="E-mail" id="" placeholder="e-mail"/>
+                    <input type="password" name="Senha" id="" placeholder="senha"  />
+                    <input type="password" name="Confirma" id="" placeholder="confirme a senha"  />
+                    
+                    <div className={styles.checkBox}>
+                        <input type="checkbox" name="Check" id=""/>
+                        <p>Quero receber promoções por e-mail.</p>
+                    </div>
+                    <button>Acessar</button>
+                </form>
+            </div>
         </div>
     )
 }
