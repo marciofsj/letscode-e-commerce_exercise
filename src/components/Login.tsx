621ca5a4927cf5318a970ad2f78d1bd7a983ed0e
marciofsj <97961755+marciofsj@users.noreply.github.com>
2022-06-22 11:59:19 -0300

    hooks

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,31 +1,89 @@
+import { useState } from 'react'
 import styles from './Login.module.css'
 
 export function Login() {
+    //para o login
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    //para o cadastro
+    const [newname, setNewName] = useState ('');
+    const [newemail, setNewEmail] = useState('');
+    const [newpassword, setNewPassword] = useState ('');
+    const [repeatpassword, setRepetNewPassword] = useState ('');
+    
+
+    const handleEnter = () => {
+        console.log(email)
+        console.log(password)
+    }
+
     return (
         <div className={styles.loginPage}>
             <div>
                 <form className={styles.loginSide}>
-                    <h2  >Login</h2>
-                    <input type="email" name="E-mail" id="" placeholder="e-mail"/>
-                    <input type="password" name="Senha" id="" placeholder="senha"  />
+                    <h2>Login</h2>
+                    <input 
+                        type="email" 
+                        name="E-mail" 
+                        placeholder="e-mail"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                    />
+
+                    <input 
+                        type="password" 
+                        name="Senha"
+                        placeholder="senha"
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}  
+                    />
                     <a href="#">Esqueceu sua senha? Clique aqui!</a>
-                    <button>Acessar</button>
+                    <button type="button" onClick={handleEnter}>Acessar</button>
                 </form>
             </div>
 
             <div>
                 <form className={styles.registerSide}>
                     <h2>Cadastro</h2>
-                    <input type="text" name="Nome" id="" placeholder="nome completo"/>
-                    <input type="email" name="E-mail" id="" placeholder="e-mail"/>
-                    <input type="password" name="Senha" id="" placeholder="senha"  />
-                    <input type="password" name="Confirma" id="" placeholder="confirme a senha"  />
+                    <input 
+                        type="text" 
+                        name="Nome" 
+                        placeholder="nome completo"
+                        value={newname}
+                        onChange={e => setNewName(e.target.value)}
+                    />
+
+                    <input 
+                        type="email" 
+                        name="E-mail" 
+                        placeholder="e-mail"
+                        value={newemail}
+                        onChange={e => setNewEmail(e.target.value)}
+                    />
+
+                    <input 
+                        type="password" 
+                        name="Senha" 
+                        placeholder="senha"
+                        value={newpassword}
+                        onChange={e => setNewPassword(e.target.value)}
+                    />
+
+                    <input 
+                        type="password" 
+                        name="Confirma" 
+                        placeholder="confirme a senha"
+                        value={repeatpassword}
+                        onChange={e => setRepetNewPassword(e.target.value)}
+                    />
+
                     
                     <div className={styles.checkBox}>
                         <input type="checkbox" name="Check" id=""/>
                         <p>Quero receber promoções por e-mail.</p>
                     </div>
-                    <button>Acessar</button>
+                    <button type="button">Acessar</button>
                 </form>
             </div>
         </div>
